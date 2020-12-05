4543a73c62b39697268ad63985b3a45b1fedab09
Azunna Ikonne <ikonnea@gmail.com>
2020-12-05 17:02:16 +0100

    Added serverless deployment

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import os
 import json 
 import requests
 
-from flask import Flask, request
+from flask_lambda import FlaskLambda
 
-app = Flask(__name__)
-app.secret_key = os.urandom(24)
+from flask import request
+
+app = FlaskLambda(__name__)
 
 # Set file upload requirements here: max file size, extension types(json and txt)
 
@@ -14,10 +15,14 @@ app.config['MAX_CONTENT_LENGTH'] = 64 * 1024 * 1024
 
 @app.errorhandler(413)
 def too_large(e):
-    return "File is too large", 413
+    return (
+            json.dumps('File is too large'),
+            413,
+            {'Content-Type': 'application/json'}
+        )
 
 def allowed_file(filename):
-    print (filename)
+    print (filename, 'Uploaded Successfully')
     if not "." in filename:
         return False
     ext = filename.rsplit(".", 1)[1]
@@ -25,14 +30,13 @@ def allowed_file(filename):
         return True
     else:
         return False
-
+      
 
 def process_upload(uploaded_file, project):
     lines = uploaded_file.readlines()
     for line in lines:
-        print (line)
         report = json.loads(line)
-        text = '*Secrets Found*\n\n*Project:*' + ' ' + project + '\n'
+        text = '*Secrets Found*\n\n*Project:* ' + project + '\n'
         for k in report:
             if k not in ('stringsFound', 'diff', 'printDiff'):
                 text += f"*{k}:* {report[k]}\n"
@@ -57,48 +61,66 @@ def process_upload(uploaded_file, project):
                         }
                     ]
                 }
-        url = "https://hooks.slack.com/services/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"
+        url = os.environ['WEBHOOK_URL']
         # Call webhook with payload
         requests.post(url, json=payload)
 
-@app.route('/', methods=['POST'])      
+@app.route('/')
+def healthcheck():
+    return (
+        json.dumps('TruffleHog Notifier'),
+        200,
+        {'Content-Type': 'application/json'}
+    )
 
+@app.route('/upload', methods=['POST'])      
 def upload_file():
     if request.method == "POST":
         
         # check if the post request has the file part
         default_project = 'Not Specified'
         project = request.form.get('project', default_project)
-
         filekey = 'file'
-        if filekey not in request.files:
-            
-            return "Unrecognized file key", 204
+
+        if filekey not in request.files:          
+            return (
+                json.dumps('Unrecognized file key'),
+                400,
+                {'Content-Type': 'application/json'}
+            )
 
         uploaded_file = request.files[filekey]
-        if uploaded_file.filename == '':
-            
-            return {
-            'statusCode': 204,
-            'body': json.dumps("No file uploaded")
-            }
 
-        if uploaded_file and allowed_file(uploaded_file.filename):
+        if uploaded_file.filename != '':  
+            if uploaded_file and allowed_file(uploaded_file.filename):
 
-            process_upload(uploaded_file, project)
-            return {
-            'statusCode': 200,
-            'body': json.dumps("File uploaded successfully")
-            }
+                process_upload(uploaded_file, project)
+                return (
+                    json.dumps('File Uploaded successfully'),
+                    200,
+                    {'Content-Type': 'application/json'}
+                )
 
-        else :
-            return {
-            'statusCode': 403,
-            'body': json.dumps("That file extension is not allowed")
-            }
+            else :
+                return (
+                    json.dumps('That file extension is not allowed. JSON only'),
+                    403,
+                    {'Content-Type': 'application/json'}
+                )
+        else:   
+            return (
+                json.dumps('No file uploaded'),
+                400,
+                {'Content-Type': 'application/json'}
+            )
+        
+        
     else :
-        return {
-        'statusCode': 405,
-        'body': json.dumps("Method not allowed")
-        }       
+        return (
+            json.dumps('Method not allowed'),
+            405,
+            {'Content-Type': 'application/json'}
+        )      
     
+if __name__ == '__main__':
+    app.run()
